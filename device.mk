b1116e0366dce29df1614405808e3f8d62cf94ee
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2020-09-05 19:29:18 +0200

    tiare: Inherit from common tree
    Change-Id: I9eb2ca59312c55d494cb18557689898d9cdf5f62

---
+++ device.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from msm8937-common
+$(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/tiare/tiare-vendor.mk)
