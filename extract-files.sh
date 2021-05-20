02b9f25a14a905c9f9f9cc43fc00da9e769cc811
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-20 23:19:23 +0800

    tiare: Add the required camera shims

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/hw/camera.msm8937.so)
+            sed -i 's|libgui.so|libwui.so|g' "${2}"
+            ;;
+        vendor/lib/libmmcamera_interface.so)
+            # Direct loading this into camera.msm8937.so causes segfault
+            if ! "${PATCHELF}" --print-needed "${2}" | grep "libshims_gui.so" >/dev/null; then
+                "${PATCHELF}" --add-needed "libshims_gui.so" "${2}"
+            fi
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
