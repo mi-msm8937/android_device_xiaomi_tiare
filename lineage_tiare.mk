0734c62b7855ed73a7e74ce1ead31ba831b4721e
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-28 10:01:08 +0800

    tiare: Update build fingerprint to V10.2.25.0.OCLMIXM

--- lineage_tiare.mk
+++ lineage_tiare.mk
@@ -27,7 +27,7 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tiare-user 8.1.0 OPM1.171019.026 V10.2.13.0.OCLMIXM release-keys"
+    PRIVATE_BUILD_DESC="tiare-user 8.1.0 OPM1.171019.026 V10.2.25.0.OCLMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/tiare/tiare:8.1.0/OPM1.171019.026/V10.2.13.0.OCLMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/tiare/tiare:8.1.0/OPM1.171019.026/V10.2.25.0.OCLMIXM:user/release-keys"
